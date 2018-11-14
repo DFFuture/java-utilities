3747fe594e0c2df30fb50e3ffeda1143cc2a0b5c
张成博 <dffuture@qq.com>
2018-11-14 15:01:33 +0800

    使用sshd实现远程执行命令

--- java-ssh/src/main/java/sshd/SshdExec.java
+++ java-ssh/src/main/java/sshd/SshdExec.java
@@ -1,22 +1,33 @@
 package sshd;
 
 import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ChannelExec;
+import org.apache.sshd.client.channel.ClientChannelEvent;
 import org.apache.sshd.client.session.ClientSession;
 
 import java.io.IOException;
+import java.util.Collections;
 
 public class SshdExec {
 
     public static void main(String[] args) throws IOException {
-        String cmd = "hostname";
+        long start = System.currentTimeMillis();
+        String cmd = "tail -f /root/apache-flume-1.8.0-bin/logs/flume.log";
         SshClient client = SshClient.setUpDefaultClient();
         client.start();
         try(ClientSession session = client
                 .connect("root", "192.168.0.111", 22)
                 .verify(3000).getSession()) {
-            session.addPasswordIdentity("12345");
+            session.addPasswordIdentity("123456");
             session.auth().verify(3000);
+            ChannelExec channel = session.createExecChannel(cmd);
+            channel.setOut(System.out);
+            channel.open();
+            //channel.waitFor(Collections.singleton(ClientChannelEvent.CLOSED), 0);
+            channel.close();
         }
+        System.out.println(System.currentTimeMillis() - start);
         client.close();
+
     }
 }
